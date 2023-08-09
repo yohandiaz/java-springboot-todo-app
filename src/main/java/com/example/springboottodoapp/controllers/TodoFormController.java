f5151a1460b58da99b9134a10ae7d2525c9a6fe8
yohandiaz <yohan.diaz5632@gmail.com>
2023-08-09 20:33:17 +0200

    adding createTodoItem functionality to TodoFormController

--- src/main/java/com/example/springboottodoapp/controllers/TodoFormController.java
+++ src/main/java/com/example/springboottodoapp/controllers/TodoFormController.java
@@ -2,7 +2,15 @@ package com.example.springboottodoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springboottodoapp.models.TodoItem;
+import com.example.springboottodoapp.services.TodoItemService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TodoFormController {
@@ -13,5 +21,15 @@ public class TodoFormController {
     @GetMapping("/create-todo")
     public String showCreateForm() {
         return "new-todo-item";
+    }
+
+    @PostMapping("/todo")
+    public String createTodoItem(@Valid TodoItem todoItem, BindingResult result, Model model) {
+        TodoItem item = new TodoItem();
+        item.setDescription(todoItem.getDescription());
+        item.setCompleted(todoItem.isCompleted());
+
+        todoItemService.save(todoItem);
+        return "redirect:/";
     }
 }
